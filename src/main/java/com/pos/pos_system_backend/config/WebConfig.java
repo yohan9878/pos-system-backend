15460468e3e7039d00c78155736ec2c180281035
Yohan Office <yohanoffice78@gmail.com>
2026-04-02 16:35:01 +0530

    Created web config file

---
+++ src/main/java/com/pos/pos_system_backend/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.pos.pos_system_backend.config;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
