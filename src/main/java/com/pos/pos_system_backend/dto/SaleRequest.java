df52575d2f075cf7b85c3f93072215df174f0716
Yohan Office <yohanoffice78@gmail.com>
2026-04-10 15:15:40 +0530

    Modified Sale Entity and Dto

--- src/main/java/com/pos/pos_system_backend/dto/SaleRequest.java
+++ src/main/java/com/pos/pos_system_backend/dto/SaleRequest.java
@@ -1,5 +1,6 @@
 package com.pos.pos_system_backend.dto;
 
+import com.pos.pos_system_backend.PriceType;
 import lombok.Data;
 import java.util.List;
 
@@ -7,10 +8,14 @@ import java.util.List;
 public class SaleRequest {
     private String invoiceNo;
     private String outletId;
-    private double total;
-    private String date;
 
     private List<SaleItem> items;
 
+    public static class Item {
+        private String barcode;
+        private int qty;
+        private double weight;
+        private PriceType priceType;
+    }
 
 }
--- src/main/java/com/pos/pos_system_backend/entity/Sale.java
+++ src/main/java/com/pos/pos_system_backend/entity/Sale.java
@@ -1,7 +1,11 @@
 package com.pos.pos_system_backend.entity;
 
+import com.pos.pos_system_backend.dto.SaleItem;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Entity
@@ -14,5 +18,10 @@ public class Sale {
     private String invoiceNo;
     private String outletId;
     private double total;
-    private String date;
+
+    private LocalDateTime date;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<SaleItem> items;
+
 }
