da0f2948a8d7707ac285b42d00d9bcb9484fbf65
Yohan Office <yohanoffice78@gmail.com>
2026-04-10 15:15:57 +0530

    Modified Sale Item Dto

--- src/main/java/com/pos/pos_system_backend/dto/SaleItem.java
+++ src/main/java/com/pos/pos_system_backend/dto/SaleItem.java
@@ -1,9 +1,26 @@
 package com.pos.pos_system_backend.dto;
 
+import com.pos.pos_system_backend.PriceType;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
 public class SaleItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private Long barcode;
-    private int qty;
+    private String productName;
+
+    private int quantity;
+    private double weight; // grams
+
+    private double unitPrice;
+    private double totalPrice;
+
+    @Enumerated(EnumType.STRING)
+    private PriceType priceType;
 }
