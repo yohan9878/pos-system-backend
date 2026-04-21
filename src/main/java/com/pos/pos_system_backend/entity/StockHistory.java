40d51385cecf4d8f7ec6cf24634415811e2824e2
Yohan Office <yohanoffice78@gmail.com>
2026-04-21 16:44:50 +0530

    Updated stock history entity

--- src/main/java/com/pos/pos_system_backend/entity/StockHistory.java
+++ src/main/java/com/pos/pos_system_backend/entity/StockHistory.java
@@ -1,5 +1,6 @@
 package com.pos.pos_system_backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -23,5 +24,6 @@ public class StockHistory {
 
     private String changedBy;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime changedAt;
 }
