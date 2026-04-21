7c15f39da07f38b388f37a5dc540671c6decd876
Yohan Office <yohanoffice78@gmail.com>
2026-04-21 16:43:05 +0530

    Updated sale repo

--- src/main/java/com/pos/pos_system_backend/repository/SaleRepository.java
+++ src/main/java/com/pos/pos_system_backend/repository/SaleRepository.java
@@ -5,10 +5,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface SaleRepository extends JpaRepository<Sale, Long> {
-    @Query("SELECT COALESCE(SUM(s.total),0) FROM Sale s WHERE s.outletId = :outletId AND s.date = :date")
-    double getTotalSales(@Param("outletId") String outletId, @Param("date") String date);
+import java.time.LocalDateTime;
+import java.util.List;
 
-    @Query("SELECT COUNT(s) FROM Sale s WHERE s.outletId = :outletId AND s.date = :date")
-    long getTotalTransactions(@Param("outletId") String outletId, @Param("date") String date);
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+    @Query("SELECT COALESCE(" + "SUM(s.total),0) " + "FROM Sale s " + "WHERE s.outletId = :outletId AND s.date BETWEEN :start AND :end")
+    double getTotalSales(@Param("outletId") String outletId, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+
+    @Query("SELECT COUNT(s) FROM Sale s WHERE s.outletId = :outletId AND s.date BETWEEN :start AND :end")
+    long getTotalTransactions(@Param("outletId") String outletId, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+
+//    List<Sale> findByDate(String date);
+//
+//    List<Sale> findByOutletIdAndDate(String outletId, String date);
+
+    @Query("SELECT DISTINCT s.outletId FROM Sale s WHERE s.date >= :start AND s.date < :end")
+    List<String> findDistinctOutletIdsBetween( @Param("start") LocalDateTime start,
+                                        @Param("end") LocalDateTime end);
 }
