b5e62fbde3a5cd01f8801e65a68afc10f426a781
yohan9878 <yohanasitha98@gmail.com>
2026-04-23 16:57:51 +0530

    Updated sale controller

--- src/main/java/com/pos/pos_system_backend/controller/SaleController.java
+++ src/main/java/com/pos/pos_system_backend/controller/SaleController.java
@@ -2,8 +2,12 @@ package com.pos.pos_system_backend.controller;
 
 
 import com.pos.pos_system_backend.dto.SaleRequest;
+import com.pos.pos_system_backend.entity.Sale;
 import com.pos.pos_system_backend.service.SaleService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/sales")
@@ -21,4 +25,21 @@ public class SaleController {
         service.processSale(request);
         return "Sale completed";
     }
+
+    @GetMapping("/daily")
+    public ResponseEntity<List<Sale>> getSalesByDateAndOutlet(
+            @RequestParam LocalDate date,
+            @RequestParam String outletId
+    ) {
+
+        List<Sale> sales = service.getSalesByDateAndOutletId(date, outletId);
+
+        return ResponseEntity.ok(sales);
+    }
+
+    @GetMapping
+    public List<Sale> getAll() {
+        return service.getAll();
+    }
+
 }
