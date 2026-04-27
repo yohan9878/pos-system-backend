f8027026b7367af4db492002c17f736e820a56a5
yohan9878 <yohanasitha98@gmail.com>
2026-04-27 16:50:38 +0530

    Updated stock history repo

--- src/main/java/com/pos/pos_system_backend/repository/StockHistoryRepository.java
+++ src/main/java/com/pos/pos_system_backend/repository/StockHistoryRepository.java
@@ -3,5 +3,10 @@ package com.pos.pos_system_backend.repository;
 import com.pos.pos_system_backend.entity.StockHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface StockHistoryRepository extends JpaRepository<StockHistory, Long> {
+    List<StockHistory> findByChangedAtAfterOrderByChangedAtDesc(LocalDateTime date);
 }
+
