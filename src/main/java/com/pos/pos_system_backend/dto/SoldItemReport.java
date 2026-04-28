45c304d7e7472e41eff71ead6352580ae7ded605
yohan9878 <yohanasitha98@gmail.com>
2026-04-28 08:13:48 +0530

    Added sales items entity

---
+++ src/main/java/com/pos/pos_system_backend/dto/SoldItemReport.java
@@ -0,0 +1,14 @@
+package com.pos.pos_system_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class SoldItemReport {
+
+        private String barcode;
+        private String itemName;
+        private double saleQty; // can be weight also
+        private double salePrice;
+        private double saleValue;
+
+}
