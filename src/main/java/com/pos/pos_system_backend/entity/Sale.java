4b93608b0efdac0a33b881a22b2fc26f2f536416
yohan9878 <yohanasitha98@gmail.com>
2026-05-19 08:47:50 +0530

    Modifies sale entity and dto

--- src/main/java/com/pos/pos_system_backend/entity/Sale.java
+++ src/main/java/com/pos/pos_system_backend/entity/Sale.java
@@ -15,7 +15,9 @@ public class Sale {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String invoiceNo;
+
     private String outletId;
     private double discountAmount;
     private double total;
